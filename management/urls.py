b9807d49d3329559e3b6c0c97e347f7500083bd9
Mat <mathewdenison@gmail.com>
2025-04-04 12:38:20 -0400

    updates

--- management/urls.py
+++ management/urls.py
@@ -3,42 +3,42 @@ from .views import EmployeeListView, login_view, logout_view, csrf_setup, Submit
 from . import views
 
 urlpatterns = [
-    path('api/user/login/', login_view, name='login'),  # Login route
-    path('api/user/logout/', logout_view, name='logout'),  # Logout route
-    path('api/user/employees/', EmployeeListView.as_view(), name='employee-list'),
-    path("api/user/csrf/", views.csrf, name="csrf"),
-    path("api/user/csrf_setup/", csrf_setup, name="csrf_setup"),
-    path('api/user/create_employee/', create_employee_page, name='create_employee'),
+    path('login/', login_view, name='login'),  # Login route
+    path('logout/', logout_view, name='logout'),  # Logout route
+    path('employees/', EmployeeListView.as_view(), name='employee-list'),
+    path("csrf/", views.csrf, name="csrf"),
+    path("csrf_setup/", csrf_setup, name="csrf_setup"),
+    path('create_employee/', create_employee_page, name='create_employee'),
 ]
 
 urlpatterns += [
-    path('api/user/employees/<int:employee_id>/submit_timesheet/', SubmitTimeLogView.as_view(), name='submit-timelog'),
+    path('employees/<int:employee_id>/submit_timesheet/', SubmitTimeLogView.as_view(), name='submit-timelog'),
 ]
 
 urlpatterns += [
-    path('api/user/employees/timelogs/', EmployeeTimeLogsView.as_view(), name='employee-timelogs'),
+    path('employees/timelogs/', EmployeeTimeLogsView.as_view(), name='employee-timelogs'),
 ]
 
 urlpatterns += [
-    path('api/user/employees/<int:employee_id>/pto/', PTOUpdateView.as_view(), name='update-pto'),
+    path('employees/<int:employee_id>/pto/', PTOUpdateView.as_view(), name='update-pto'),
 ]
 
 urlpatterns += [
-    path('api/user/employees/<int:employee_id>/get_timesheet/', TimeLogListView.as_view(), name='get-timelog'),
+    path('employees/<int:employee_id>/get_timesheet/', TimeLogListView.as_view(), name='get-timelog'),
 ]
 
 urlpatterns += [
-    path('api/user/ptoBalance/', GetPTOView.as_view(), name='get-pto'),
+    path('ptoBalance/', GetPTOView.as_view(), name='get-pto'),
 ]
 
 urlpatterns += [
-    path('api/user/payPeriod/', CurrentWeekView.as_view(), name='get-payperiod'),
+    path('payPeriod/', CurrentWeekView.as_view(), name='get-payperiod'),
 ]
 
 urlpatterns += [
-    path('api/user/timelogs/<int:pk>/', views.TimeLogUpdateView.as_view(), name='update-timelog'),
+    path('timelogs/<int:pk>/', views.TimeLogUpdateView.as_view(), name='update-timelog'),
 ]
 
 urlpatterns = [
-    path('api/user/verify-token/', verify_token, name='verify_token'),  # Add this line
+    path('verify-token/', verify_token, name='verify_token'),  # Add this line
 ]
--- user_management_service/urls.py
+++ user_management_service/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('api/user/admin', admin.site.urls),
+    path('api/user/', include('manage.urls')),
 ]
