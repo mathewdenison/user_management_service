17dcaa5d54e65f8946679b4bc389086581a5eed0
Mat <mathewdenison@gmail.com>
2025-04-04 13:46:39 -0400

    updates

--- user_management_service/urls.py
+++ user_management_service/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/user/admin', admin.site.urls),
+    path('api/user/admin/', admin.site.urls),
     path('api/user/', include('management.urls')),
 ]
