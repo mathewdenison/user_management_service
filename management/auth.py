1625b892631824b872b978494335ef08c999248c
Mat <mathewdenison@gmail.com>
2025-04-16 17:10:37 -0400

    Updates

--- management/auth.py
+++ management/auth.py
@@ -1,19 +1,18 @@
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
-from management.models import Employee  # Your Firestore Employee model
+from management.models import Employee
 
 class FirestoreEmployeeAuth(BaseAuthentication):
     def authenticate(self, request):
         username = request.headers.get("X-Username")
 
         if not username:
-            return None  # Let other authenticators try (or treat as unauthenticated)
+            return None
 
         employee = Employee.get_by_username(username)
         if not employee:
             raise AuthenticationFailed("Invalid employee or user not found.")
 
-        # Use a dummy user object or AnonymousUser if you aren't using Django's auth system
         user = type("User", (object,), {"is_authenticated": True})()
         user.employee = employee
         return (user, None)
